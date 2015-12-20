03f3960324ac76e56701a88eb24951bde8df9c1e
wen <1165737969@qq.com>
2015-12-21 06:12:14 +0800

    update

---
+++ vendor/autoload.php
@@ -0,0 +1,5 @@
+<?php
+require_once __DIR__ . '/Workerman/Autoloader.php';
+require_once __DIR__ . '/Composer' . '/autoload_real.php';
+
+return ComposerAutoloaderInit664c71ec9202e3c8c4529be9fc9b2295::getLoader();
